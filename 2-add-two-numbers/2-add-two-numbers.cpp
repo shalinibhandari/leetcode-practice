b70c80e0b69c462854f61dd26356ce53b0fd376c
Shalini <54896093+shalinibhandari@users.noreply.github.com>
2022-08-23 12:05:18 +0530

    Time: 82 ms (12.40%), Space: 71.5 MB (51.41%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -11,72 +11,35 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        vector<int>a;
-        vector<int>b;
-        if(l1==NULL and l2!=NULL)  return l2;
-        if(l2==NULL and l1!=NULL) return l1;
-        while(l1)
+        ListNode*ans=new ListNode();
+        ListNode*dummy=ans;
+        int carry=0;
+        
+        while(l1 || l2)
         {
-            a.push_back(l1->val);
-            l1=l1->next;
-
-        }
-        while(l2)
-        {
-            b.push_back(l2->val);
-            l2=l2->next;
-        }
-       reverse(a.begin(),a.end());
-        reverse(b.begin(),b.end());
-        ListNode*h;
-        int i=a.size()-1,j=b.size()-1;
-        int c=0,carry=0;
-        ListNode*ans;
-        while(i>=0 and j>=0)
-        {
-            int p=a[i]+b[j]+carry;
-            --i;
-            --j;
-            
-            ListNode*n=new ListNode(p%10);
-            carry=p/10;
-            if(c==0)
+            int sum=0;
+            if(l1)
             {
-                h=n;
-                ans=n;
-                c=1;
+                sum+=l1->val;
+                l1=l1->next;
             }
-            else
+            if(l2)
             {
-                h->next=n;
-                h=n;
+                sum+=l2->val;
+                l2=l2->next;
             }
+            sum+=carry;
+            ListNode*n=new ListNode(sum%10);
+            carry=sum/10;
+            dummy->next=n;
+            dummy=dummy->next;
         }
-        while(i==-1 and j>=0)
+        if(carry>0)
         {
-            int p=b[j]+carry;
-            --j;
-            ListNode*n=new ListNode(p%10);
-            carry=p/10;
-            h->next=n;
-            h=n;
-        }
-        while(j==-1 and i>=0)
-        {
-            int p=a[i]+carry;
-            --i;
-            ListNode*n=new ListNode(p%10);
-            carry=p/10;
-            h->next=n;
-            h=n;
-        }
-        if(carry!=0)
-        {
-             ListNode*n=new ListNode(carry%10);
+            ListNode*n=new ListNode(carry);
+            dummy->next=n;
             
-            h->next=n;
-            h=n;
         }
-        return  ans;
+        return ans->next;
     }
 };
